f824308ea702f0beb1af6cbfd53444111143426e
Achan <781290982@qq.com>
2018-10-09 19:18:49 +0800

    fix # init this project. Signed-off-by: Achan <781290982@qq.com>

---
+++ app/src/main/java/com/warchaser/sealtest/App.java
@@ -0,0 +1,14 @@
+package com.warchaser.sealtest;
+
+import android.app.Application;
+
+import io.rong.imkit.RongIM;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        RongIM.init(this);
+    }
+}
